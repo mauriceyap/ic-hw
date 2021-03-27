c12e1dc371510a3f3aedc97c57d28ba343a4f36d
Maurice Yap <maurice@maurice-tm05268-mbp.nomad.thomac.net>
2021-03-27 15:26:07 +0000

    tweaks

--- src/pages/LivePrice.js
+++ src/pages/LivePrice.js
@@ -3,20 +3,27 @@ import {useState} from 'react'
 import {Prices} from '../components/Prices'
 
 export function LivePricePage() {
-  const [gbp, setGbp] =useState(null)
+  const [gbp, setGbp] = useState(null)
+  const [eur, setEur] = useState(null)
+  const [usd, setUsd] = useState(null)
 
   function refreshPrices() {
     fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
     .then((response) => response.json())
     .then((json) => {
       setGbp(json.bpi.GBP.rate)
+      setEur(json.bpi.EUR.rate)
+      setUsd(json.bpi.USD.rate)
     })
   }
 
   return (
   <div>
     <h1>Live bitcoin price</h1>
-    <Prices usd={123} eur={1234} gbp={gbp} />
+    {
+      !gbp && !eur && !usd ? <p>please click refresh to get prices</p> : <Prices usd={usd} eur={eur} gbp={gbp} />
+    }
+    
     <button onClick={refreshPrices}>Refresh prices</button>
   </div>
   );
